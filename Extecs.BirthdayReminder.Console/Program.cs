6a3f11ed7f97d369d9e5b482e73e6eddf0eaf711
Tono Novoa <tono_novoa@hotmail.com>
2024-04-08 17:08:55 -0600

    Agregar archivos de proyecto.

---
+++ Extecs.BirthdayReminder.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extecs.BirthdayReminder.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
